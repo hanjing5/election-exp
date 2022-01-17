c0f95dd17592fca6d5b2432f63648c6bc1ac600b
Han Jing <hanjing@berkeley.edu>
2022-01-16 21:25:03 -0800

    update config to point to binance testnet

--- truffle-config.js
+++ truffle-config.js
@@ -38,7 +38,7 @@ module.exports = {
       gasPrice: 470000000000,
       skipDryRun: true
     },
-    testnet: {
+    binance_testnet: {
       provider: () => new HDWalletProvider(privateKeys, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
