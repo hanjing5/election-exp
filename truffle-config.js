a7dc6ac10f6acc0b1a89db4ef5cf863380eb8472
Han Jing <hanjing@berkeley.edu>
2022-01-16 18:45:10 -0800

    update contract code and compiler version. start first contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+  solc: {
+    version: '0.5.16'
+  },
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      port: 8545,
+      network_id: "1337" // use "*" to match any network id 
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.ROPSTEN_API_KEY}`,
